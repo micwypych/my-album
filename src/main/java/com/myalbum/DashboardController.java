e591528a1479222b81b2cbdcb67f9a773ecfab47
Michał Wypych <micwypych@gmail.com>
2017-07-26 16:01:17 +0200

    Adds HATEOS support

--- src/main/java/com/myalbum/DashboardController.java
+++ src/main/java/com/myalbum/DashboardController.java
@@ -1,18 +1,28 @@
 package com.myalbum;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 /**
  * Created by mwypych on 14.07.17.
  */
 @RestController
 public class DashboardController {
-    @RequestMapping("/dashboard")
-    public Greeting dashboard(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(String.format(greetingMessage, name));
+    @GetMapping("/dashboard")
+    public HttpEntity<Greeting> dashboard(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+
+        Greeting greeting = new Greeting(String.format(greetingMessage, name));
+        greeting.add(linkTo(methodOn(DashboardController.class).dashboard(name)).withSelfRel());
+        return new ResponseEntity<Greeting>(greeting, HttpStatus.OK);
     }
 
     final String greetingMessage = "Hello, %s!";
--- src/main/java/com/myalbum/Greeting.java
+++ src/main/java/com/myalbum/Greeting.java
@@ -1,16 +1,19 @@
 package com.myalbum;
+import org.springframework.hateoas.ResourceSupport;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * Created by mwypych on 14.07.17.
  */
-public class Greeting {
+public class Greeting extends ResourceSupport {
 
-    public Greeting(String message) {
+    @JsonCreator
+    public Greeting(@JsonProperty("greeting") String message) {
         this.message = message;
     }
 
-    @JsonProperty(value = "gretting")
+    @JsonProperty(value = "greeting")
     private final String message;
 }
